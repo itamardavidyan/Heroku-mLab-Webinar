573a3b7bfde492a9fbb6a9e73692f148af2fec6d
Itamar Davidyan <itamardavidyan@gmail.com>
2018-09-17 00:13:40 +0300

    first deploy

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
-var MongoClient = require('mongodb').MongoClient;
-var mongoDBurl = "mongodb://itamard:Google100!@ds229312.mlab.com:29312/forms";
+const MongoClient = require('mongodb').MongoClient;
+const mongoDBurl = "mongodb://itamard:Google100!@ds229312.mlab.com:29312/forms";
+const path = require('path');
 
 app.get('/', function(req,res) {
 	console.log("/");
@@ -13,52 +14,52 @@ app.get('/index', function(req,res) {
 	res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
-app.get('/setTable', function(req, res) {
-    console.log('/setTable');
-    MongoClient.connect(mongoDBurl, function(err, db) {
-		if (err) throw err;
-		var dbo = db.db("forms");
+// app.get('/setTable', function(req, res) {
+//     console.log('/setTable');
+//     MongoClient.connect(mongoDBurl, function(err, db) {
+// 		if (err) throw err;
+// 		var dbo = db.db("forms");
 
-		dbo.collection("forms").find({}).sort({form_id: 1}).toArray(function(err, result) {
-			if (err) throw err;
-			res.send(result);
-			db.close();
-		});
-	});
-});
+// 		dbo.collection("forms").find({}).sort({form_id: 1}).toArray(function(err, result) {
+// 			if (err) throw err;
+// 			res.send(result);
+// 			db.close();
+// 		});
+// 	});
+// });
 
-app.post('/save', function(req, res) {
-    console.log('/save');
-    MongoClient.connect(mongoDBurl, function(err, db) {
-		if (err) throw err;
-		var dbo = db.db("forms");
-		var lastID;
+// app.post('/save', function(req, res) {
+//     console.log('/save');
+//     MongoClient.connect(mongoDBurl, function(err, db) {
+// 		if (err) throw err;
+// 		var dbo = db.db("forms");
+// 		var lastID;
 
-		dbo.collection('forms', function(err, collection) {
-			collection
-				.find()
-				.sort({form_id: -1})
-				.limit(1)
-				.next()
-				.then(
-				function(doc) {
-					lastID = doc.form_id;
-					lastID++;
+// 		dbo.collection('forms', function(err, collection) {
+// 			collection
+// 				.find()
+// 				.sort({form_id: -1})
+// 				.limit(1)
+// 				.next()
+// 				.then(
+// 				function(doc) {
+// 					lastID = doc.form_id;
+// 					lastID++;
 
-					var myobj = { form_id: lastID , form_name: req.body.formname, num_submissions:0, fields: JSON.parse(req.body.tabledata), submissions: JSON.parse(req.body.inputNames) };
+// 					var myobj = { form_id: lastID , form_name: req.body.formname, num_submissions:0, fields: JSON.parse(req.body.tabledata), submissions: JSON.parse(req.body.inputNames) };
 
-					dbo.collection("forms").insertOne(myobj, function(err, res) {
-						if (err) throw err;
-						db.close();
-					});
-				},
-				function(err) {
-					console.log('Error:', err);
-				}
-			);
-		});
-	});
-});
+// 					dbo.collection("forms").insertOne(myobj, function(err, res) {
+// 						if (err) throw err;
+// 						db.close();
+// 					});
+// 				},
+// 				function(err) {
+// 					console.log('Error:', err);
+// 				}
+// 			);
+// 		});
+// 	});
+// });
 
 const PORT = process.env.PORT || 3000;
 // const PORT = 8080;
